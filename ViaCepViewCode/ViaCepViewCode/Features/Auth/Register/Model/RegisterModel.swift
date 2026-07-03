e814716aeed3274c983d0e49aed239cf5da5213d
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-07-03 15:37:29 -0300

    feat: create RegisterModel file in Features/Auth/Register/Model folder

---
+++ ViaCepViewCode/ViaCepViewCode/Features/Auth/Register/Model/RegisterModel.swift
@@ -0,0 +1,13 @@
+//
+//  RegisterModel.swift
+//  ViaCepViewCode
+//
+//  Created by Gabriel Merenfeld on 03/07/26.
+//
+
+struct RegisterModel {
+    var name: String
+    var email: String
+    var password: String
+    var confirmPassword: String
+}
