843f380936c724f287846d9503e5ae8d929f6619
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-07-04 13:00:42 -0300

    feat: create RegisterScreenDelegate protocol in Features/Auth/Register/View/Delegate folder

---
+++ ViaCepViewCode/ViaCepViewCode/Features/Auth/Register/View/Delegate/RegisterScreenDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  RegisterScreenDelegate.swift
+//  ViaCepViewCode
+//
+//  Created by Gabriel Merenfeld on 04/07/26.
+//
+
+protocol RegisterScreenDelegate: AnyObject {
+    func didTapPrimaryButton()
+    func didTapSecondaryButton()
+}
