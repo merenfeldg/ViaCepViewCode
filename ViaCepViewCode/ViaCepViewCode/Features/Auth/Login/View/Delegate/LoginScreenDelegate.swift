b7803a1c529a45b6fae02283f007e4d23f2fbada
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-07-04 13:02:52 -0300

    feat: create LoginScreenDelegate protocol in Features/Auth/Login/View/Delegate folder

---
+++ ViaCepViewCode/ViaCepViewCode/Features/Auth/Login/View/Delegate/LoginScreenDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  LoginScreenDelegate.swift
+//  ViaCepViewCode
+//
+//  Created by Gabriel Merenfeld on 04/07/26.
+//
+
+protocol LoginScreenDelegate: AnyObject {
+    func didTapPrimaryButton()
+    func didTapSecondaryButton()
+}
