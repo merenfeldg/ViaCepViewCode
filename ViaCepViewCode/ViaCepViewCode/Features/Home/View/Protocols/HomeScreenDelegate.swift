4cb9e4c391edb321a7fb39e3a6dc57ad7c6ea7c8
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-07-14 14:11:06 -0300

    feat: create HomeScreenDelegate file in Features/Home/View/Protocols folder

---
+++ ViaCepViewCode/ViaCepViewCode/Features/Home/View/Protocols/HomeScreenDelegate.swift
@@ -0,0 +1,10 @@
+//
+//  HomeScreenDelegate.swift
+//  ViaCepViewCode
+//
+//  Created by Gabriel Merenfeld on 14/07/26.
+//
+
+protocol HomeScreenDelegate: AnyObject {
+    func searchCEP(_ cep: String)
+}
