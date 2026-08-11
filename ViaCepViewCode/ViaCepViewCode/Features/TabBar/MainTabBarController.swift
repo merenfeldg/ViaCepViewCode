59d705741d2f66f96169c4f3f7bb1fe8fdc925b7
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-08-11 14:31:24 -0300

    feat: create MainTabBarController and disable liquid glass in info.plist

---
+++ ViaCepViewCode/ViaCepViewCode/Features/TabBar/MainTabBarController.swift
@@ -0,0 +1,64 @@
+//
+//  MainTabBarController.swift
+//  ViaCepViewCode
+//
+//  Created by Gabriel Merenfeld on 11/08/26.
+//
+
+import UIKit
+
+final class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        createTabBarItem(
+            viewController: HomeViewController(),
+            title: "Buscar",
+            imageString: "magnifyingglass"
+        )
+        
+        createTabBarItem(
+            viewController: UIViewController(),
+            title: "Histórico",
+            imageString: "clock.arrow.circlepath"
+        )
+        
+        customizeApperance()
+    }
+    
+    private func customizeApperance() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithDefaultBackground()
+        appearance.backgroundColor = .white
+        appearance.shadowColor = .separator
+    
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = appearance
+        tabBar.unselectedItemTintColor = .lightGray
+        tabBar.tintColor = .black
+    }
+    
+    private func createTabBarItem(
+        viewController: UIViewController,
+        title: String,
+        imageString: String
+    ) {
+        let navController = UINavigationController(
+            rootViewController: viewController
+        )
+        
+        navController.tabBarItem = UITabBarItem(
+            title: title,
+            image: UIImage(systemName: imageString),
+            selectedImage: UIImage(systemName: imageString)
+        )
+        
+        addNavControllerInTabBar(navController)
+    }
+    
+    private func addNavControllerInTabBar(_ navController: UINavigationController) {
+        if viewControllers == nil {
+            viewControllers = [navController]
+        } else {
+            viewControllers?.append((navController))
+        }
+    }
+}
