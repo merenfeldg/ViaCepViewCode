9e27e963ff8db7afc7117d5caea8e9712511b0ef
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-08-13 13:59:17 -0300

    feat: create UserDefaultsManager in Shared/Helpers/UserDefaults folder

---
+++ ViaCepViewCode/ViaCepViewCode/Shared/Helpers/UserDefaults/UserDefaultsManager.swift
@@ -0,0 +1,36 @@
+//
+//  UserDefaultsManager.swift
+//  ViaCepViewCode
+//
+//  Created by Gabriel Merenfeld on 13/08/26.
+//
+
+import Foundation
+
+enum UserDefaultsKey: String {
+    case isUserLoggedIn = "isUserLoggedIn"
+    case email = "userEmail"
+    case name = "userName"
+}
+
+final class UserDefaultsManager {
+    static let shared = UserDefaultsManager()
+    
+    private let userDefauts: UserDefaults
+    
+    init(userDefauts: UserDefaults = .standard) {
+        self.userDefauts = userDefauts
+    }
+    
+    func save<T>(_ value: T, forKey key: UserDefaultsKey) {
+        userDefauts.setValue(value, forKey: key.rawValue)
+    }
+    
+    func getString(forKey key: UserDefaultsKey) -> String? {
+        return userDefauts.string(forKey: key.rawValue)
+    }
+    
+    func getBool(forKey key: UserDefaultsKey) -> Bool {
+        return userDefauts.bool(forKey: key.rawValue)
+    }
+}
